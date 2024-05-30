e2039e351c0cb0e9a46bc84aff6be69b47f2c5fb
Sean Steiger-McRae <seanste@dor.wa.gov>
2024-05-30 16:27:30 -0700

    Build and return blob SAS token

--- SasTokenFunc.cs
+++ SasTokenFunc.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
 using Microsoft.Extensions.Logging;
 
 namespace sowa.dor
@@ -18,9 +20,22 @@ namespace sowa.dor
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            var connectionString = Environment.GetEnvironmentVariable("AzureStorage") ?? "hello world";
+            var connectionString = Environment.GetEnvironmentVariable("AzureStorage");
+            var containerName = Environment.GetEnvironmentVariable("ContainerName");
 
-            return new OkObjectResult(connectionString);
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference(containerName);
+
+            SharedAccessBlobPolicy accessPolicy = new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(30),
+                Permissions = SharedAccessBlobPermissions.Create | SharedAccessBlobPermissions.Write
+            };
+
+            var token = container.GetSharedAccessSignature(accessPolicy);
+
+            return new OkObjectResult($"{container.Uri}{token}");
         }
     }
 }
